4ebad28d042121c8c291d9d89fe4aeffe7d78a65
MohammedShetaya <mohammedshetaya22@gmail.com>
2023-08-24 05:51:45 +0300

    fix: provider.tf

--- infra/development/provider.tf
+++ infra/development/provider.tf
@@ -1,12 +1,11 @@
 terraform {
-  required_providers {
-    backend "remote" {
-      organization = "Shetaya_org"
-      workspaces {
-        name = "IaC"
-      }
+  backend "remote" {
+    organization = "Shetaya_org"
+    workspaces {
+      name = "IaC"
     }
-
+  }
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
