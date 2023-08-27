4f689219f0bed286f2931e64bab6a711e634f00a
MohammedShetaya <mohammedshetaya22@gmail.com>
2023-08-27 09:53:49 +0300

    fix: running server using a script

--- api/run_server.sh
+++ api/run_server.sh
@@ -4,9 +4,12 @@ set -x
 
 if ! command -v node &> /dev/null; then
     echo "Node.js is not installed. Installing..."
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
+    echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/nodesource.list > /dev/null
+    echo "deb-src [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list > /dev/null
+
     sudo apt update -y
-    sudo apt install nodejs -y
-    sudo apt install npm -y
+    sudo apt install -y nodejs 
 
     echo "Node.js is now installed."
 else
