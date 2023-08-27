1144ce50f307d8a13595554800b879fba5d82db4
MohammedShetaya <mohammedshetaya22@gmail.com>
2023-08-27 16:32:04 +0300

    fix: node version

--- api/run_server.sh
+++ api/run_server.sh
@@ -6,8 +6,13 @@ if ! command -v node &> /dev/null; then
     echo "Node.js is not installed. Installing..."
 
     sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt install -y curl
+
+    # add nodejs repository 
+    curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+
     sudo apt install -y nodejs 
-    sudo apt install -y npm
 
     echo "Node.js is now installed."
 else
